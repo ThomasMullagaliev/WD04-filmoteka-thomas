50cd81e4cc1856862654b6a43e167cd56014545c
Thomas <thomas91@list.ru>
2018-10-01 13:04:15 +0300

    Two

--- index.php
+++ index.php
@@ -156,7 +156,7 @@
 	<script src="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.js"></script>
 	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIr67yxxPmnF-xb4JVokCVGgLbPtuqxiA"></script><!-- endbuild -->
 	<!-- build:jsMain js/main.js -->
-	<!-- <script src="js/main.js"></script><!-- endbuild --> -->
+	<!-- <script src="js/main.js"></script><! endbuild -->
 	<script defer="defer" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </body>
 
